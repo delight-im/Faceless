63c988cb4f648d5cbc27cd42d8a6909d4b0838bb
Marco <marco1@live.de>
2014-07-17 18:12:56 +0200

    Changed encryption algorithm from Blowfish to AES

--- Server/base_crypto.php
+++ Server/base_crypto.php
@@ -3,13 +3,21 @@
 require_once(__DIR__.'/public_html/classes/Encryption.php');
 
 function encrypt($data, $messageSecret) {
-    $e = new Encryption(MCRYPT_BLOWFISH, MCRYPT_MODE_CBC);
+    $e = new Encryption(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
     return $e->encrypt($data, createKey($messageSecret));
 }
 
 function decrypt($data, $messageSecret) {
-    $e = new Encryption(MCRYPT_BLOWFISH, MCRYPT_MODE_CBC);
-    return $e->decrypt($data, createKey($messageSecret));
+    $e = new Encryption(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+    $res = $e->decrypt($data, createKey($messageSecret));
+    if ($res !== false) {
+        return $res;
+    }
+    else {
+        // delete this branch after some transition period
+        $e = new Encryption(MCRYPT_BLOWFISH, MCRYPT_MODE_CBC);
+        return $e->decrypt($data, createKey($messageSecret));
+    }
 }
 
 ?>
